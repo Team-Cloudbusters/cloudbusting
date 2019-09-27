e073292901b8f686a0fc8e5f32688202940b6de0
Godfrey Leung <godfrey@godfreys-mbp-2.home>
2019-09-27 02:10:45 +0100

    Modified the code to increase efficiency - GL

--- src/EDA/get_train_info.py
+++ src/EDA/get_train_info.py
@@ -1,10 +1,11 @@
-from collections import defaultdict
+from collections import Counter
+import pandas as pd
 import re
 
-def get_number_per_image_id(arr):
+def get_class_per_image(arr):
     """
-    Function that returns the number of entries per image id in the training set
-     of (cloud) images, ignoring the missing entries.
+    Function that returns the classified (cloud) labels per images in the
+    training set of (cloud) images.
 
     Parameters
     ----------
@@ -20,16 +21,23 @@ def get_number_per_image_id(arr):
 
     Examples
     --------
-    >>> arr1 = pd.Series({'A': ["001.jpg_Fish", "001.jpg_Sugar", "002.jpg_Flower"]})
-    >>> get_number_per_image_id(arr1.A)
-    defaultdict(int, {'001': 2, '002': 1})
+    >>> arr = pd.DataFrame({'A': ["001.jpg_Fish", "001.jpg_Sugar", "002.jpg_Flower"]})
+    >>> get_class_per_image(arr.A)
+              cloud_label
+    image_id             
+    001       Fish, Sugar
+    002            Flower
 
     """
-    number_per_image_id = defaultdict(int)
-    for image_label in arr:
-        image_id = str.split(image_label, '.')[0]
-        number_per_image_id[image_id] += 1
-    return number_per_image_id
+    image_id = arr.apply(lambda x: str.split(x, '.')[0])
+    class_per_entries = arr.apply(lambda x: str.split(x, '_')[1])
+    df_entries = pd.DataFrame({'image_id': image_id, 'cloud_label': class_per_entries})
+    df_class_per_image = pd.pivot_table(df_entries, index=['image_id'], columns=[],
+                                        values=['cloud_label'],
+                                        aggfunc = lambda x: ' '.join(x))
+    df_class_per_image['cloud_label'] = \
+        df_class_per_image.cloud_label.apply(lambda x: x.strip(' ').replace(' ', ', '))
+    return df_class_per_image
 
 
 def get_number_per_class_label(arr):
@@ -51,15 +59,16 @@ def get_number_per_class_label(arr):
 
     Examples
     --------
-    >>> arr1 = pd.Series({'A': ["1.jpg_Fish", "1.jpg_Sugar", "2.jpg_Flower"]})
-    >>> get_number_per_image_id_per_class_label(arr1.A)
-    {'Fish': 1, 'Flower': 1, 'Gravel': 0, 'Sugar': 1}
+    >>> arr = pd.DataFrame({'A': ["1.jpg_Fish", "1.jpg_Sugar", "2.jpg_Flower"]})
+    >>> get_number_per_class_label(arr.A)
+    Counter({'Fish': 1, 'Sugar': 1, 'Flower': 1})
 
     """
-    number_per_labels = {"Fish":0, "Flower":0, "Gravel":0, "Sugar":0}
-    for image_label in arr:
-        for label in number_per_labels.keys():
-            if re.search(label, image_label):
-                number_per_labels[label] += 1
-                break
+    class_per_entries = arr.apply(lambda x: str.split(x, '_')[1])
+    number_per_labels = Counter(class_per_entries)
     return number_per_labels
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
