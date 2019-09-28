8b94d0aa57697f6fef1b244971e34991bb5c6c98
David Carton <cartondj@gmail.com>
2019-09-29 01:43:27 +0200

    add run length to mask conversion

--- lib/cloudbusting/utils.py
+++ lib/cloudbusting/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 def rle_to_mask(rle, shape):
     """Convert run length encoded sequence to binary mask
@@ -22,5 +23,12 @@ def rle_to_mask(rle, shape):
     offsets = rle[::2]
     lengths = rle[1::2]
 
+    mask = np.zeros(shape[::-1], dtype=bool)
+    mask_flat = mask.ravel()
+
+    for offset, length in zip(offsets, lengths):
+        mask_flat[offset-1:offset+length-1] = True
+
+    mask = mask.T
 
     return mask
