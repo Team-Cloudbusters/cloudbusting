c85d0645e1e7962182c84e481e2f2ab1859f2e67
David Carton <cartondj@gmail.com>
2019-09-29 15:35:06 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open('requirements.txt', 'r') as fh:
+    install_requirements = fh.readlines()
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cloudbusting",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+#    author="Example Author",
+#    author_email="author@example.com",
+    description="Understanding cloud organization",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Team-Cloudbusters/cloudbusting',
+    package_dir={'': 'lib'},
+    packages=setuptools.find_packages('lib'),
+    install_requires=install_requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Science/Research",
+        #"Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
