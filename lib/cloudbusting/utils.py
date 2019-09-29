4f31cf2fecb730444c7edf48de1531dec4dd5f51
David Carton <cartondj@gmail.com>
2019-09-29 15:01:28 +0200

    add code to read train data

--- lib/cloudbusting/utils.py
+++ lib/cloudbusting/utils.py
@@ -1,4 +1,32 @@
+from pathlib import Path
+import logging
+
 import numpy as np
+import pandas as pd
+
+def get_train_mask(image_id, cloud_type, dir_data):
+
+    logger = logging.getLogger(__name__)
+
+    dir_data = Path(dir_data)
+
+    file = dir_data / 'train.csv'
+
+    df = pd.read_csv(file, index_col='Image_Label')
+
+    idx = '{}.jpg_{}'.format(image_id, cloud_type)
+    rle = df.loc[idx,'EncodedPixels']
+
+    if isinstance(rle, str):
+        rle = [int(i) for i in rle.split()]
+    else: #i.e. is NaN
+        rle = []
+
+    shape = (1400, 2100)
+    mask = rle_to_mask(rle, shape)
+
+    return mask
+
 
 def rle_to_mask(rle, shape):
     """Convert run length encoded sequence to binary mask
